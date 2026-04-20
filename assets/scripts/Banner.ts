120fe2a02553bf865757e675b42a7db0c4db3893
scorpio888 <219079880+scorpio888@users.noreply.github.com>
2026-04-20 10:31:00 +0800

    打包

--- assets/scripts/Banner.ts
+++ assets/scripts/Banner.ts
@@ -8,22 +8,22 @@ export default class Banner extends cc.Component {
   static RegionMask: boolean = false; //地区判断.true为有广告，false为无广告（不需要再此处手动修改，所有广告修改前往BannerManager）
   static IsLogin: boolean = false;
   private FreeInfo: number = 0; //免费跳过广告次数
-  static Owner: string = `著作权人：厦门魔芋互娱科技有限公司`; //健康忠告-著作权人
-  static License: string = `登记号:2024SR0338991`; //健康忠告-登记号
+  static Owner: string = `著作权人：厦门来日方长信息科技有限公司`; //健康忠告-著作权人
+  static License: string = `登记号:2026SA0054418`; //健康忠告-登记号
   static AgeLimit: number = 12; //健康忠告-适龄
 
-  static Company: string = `厦门索润网络科技有限公司`; //公司
-  static Email: string = `gamekf_666@sina.com`; //联系邮箱
+  static Company: string = `厦门来日方长信息科技有限公司`; //公司
+  static Email: string = `3902543256@qq.com`; //联系邮箱
 
   _appId: string = "115645129";
-  _videoId: string = "8qg06d7bkag1ome140";
-  _customId: string = "";
-  _bannerId: string = "";
+  _videoId: string = "3903175";
+  _customId: string = "3903156";
+  _bannerId: string = "3903155";
   _carouselBoxId: string = ""; // 轮播盒子广告ID
 
-  _year: number = 2025;
-  _month: number = 10;
-  _date: number = 15;
+  _year: number = 2026;
+  _month: number = 4;
+  _date: number = 21;
   _hour: number = 19;
 
   nativeAd = null; //原生广告
@@ -542,7 +542,7 @@ export default class Banner extends cc.Component {
       this.customAd = qg.createCustomAd({
         posId: Banner.Instance._customId,
         style: {
-          top: qg.getSystemInfoSync().screenHeight / 2 - 120,
+          top: 300,
           // left: 0,
           // width: qg.getSystemInfoSync().screenWidth,
         },
